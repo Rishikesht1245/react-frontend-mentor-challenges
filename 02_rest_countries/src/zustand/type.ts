093f6ae2be1f43bc0f2138c3b4fa0260c6dcb55a
Rishikesht1245 <rishikeshtgit@gmail.com>
2024-10-23 14:17:10 +0530

    Update : created zustand store types for rest countries project.

---
+++ 02_rest_countries/src/zustand/type.ts
@@ -0,0 +1,36 @@
+export type Country = {
+  flags: {
+    png: string;
+    svg: string;
+    alt: string;
+  };
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      eng: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  capital: string[];
+  region: string;
+  population: number;
+};
+
+export type CountryStore = {
+  isLoading: boolean;
+  error: string;
+  countries: Country[];
+  loadCountries: () => Promise<void>;
+};
+
+// these are the type we need to use in the set fucntion of zustand store. (these are the arguments which we can pass and return types)
+export type StoreSet = (
+  partial:
+    | CountryStore
+    | Partial<CountryStore>
+    | ((state: CountryStore) => CountryStore | Partial<CountryStore>),
+  replace?: boolean | undefined
+) => void;
